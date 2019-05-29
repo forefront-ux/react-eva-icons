c9bf1220bdaa22c3f8ecb2e024c610f570e2984c
Kuma Li <li.jun.kuma@gmail.com>
2019-05-29 14:09:35 +0800

    [docs]: add hero and sample code

--- web/preview/pages/index.js
+++ web/preview/pages/index.js
@@ -10,6 +10,7 @@ import { EvaGithub, EvaTwitter } from '@forefront-ux/react-eva-icons';
 import compose from '../utils/compose';
 import Head from '../components/Head';
 import Icons from '../components/Icons';
+import '@forefront-ux/react-eva-icons/index.css';
 
 const styles = theme => ({
   root: {
@@ -50,6 +51,56 @@ const styles = theme => ({
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2),
   },
+  hero: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  heroTitle: {
+    color: '#3F51B5',
+    fontSize: '72px',
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  version: {
+    color: '#0FBA81',
+    marginLeft: theme.spacing(3),
+    fontSize: '20px',
+    display: 'inline-block',
+    backgroundColor: '#EDFFF3',
+    padding: '4px 10px',
+  },
+  heroDesc: {
+    color: '#555',
+    fontSize: '20px',
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  tutorial: {
+    marginTop: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    fontSize: '18px'
+  },
+  tutorialBlock: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  code: {
+    paddingTop: '10px',
+    paddingBottom: '10px',
+    marginRight: theme.spacing(3),
+    fontSize: '18px',
+    fontWeight: 'bold',
+  },
+  pre: {
+    backgroundColor: '#ccc',
+    fontSize: '14px',
+    fontWeight: 'normal',
+    padding: '10px',
+  },
 });
 
 const App = props => {
@@ -74,7 +125,7 @@ const App = props => {
               data-ga-event-category="AppBar"
               data-ga-event-action="Twitter"
             >
-              <EvaTwitter color="#fff" />
+              <EvaTwitter color="#fff" animation="pulse" />
             </IconButton>
           </Tooltip>
           <Tooltip title="GitHub" enterDelay={300}>
@@ -87,13 +138,85 @@ const App = props => {
               data-ga-event-category="AppBar"
               data-ga-event-action="GitHub"
             >
-              <EvaGithub color="#fff" />
+              <EvaGithub color="#fff" animation="pulse" />
             </IconButton>
           </Tooltip>
         </Toolbar>
       </AppBar>
       <main className={classes.main}>
         <AppBar className={classes.appBarPlaceHolder}>Place Holder</AppBar>
+        <div className={classes.hero}>
+          <h1 className={classes.heroTitle}>React Eva Icons <span className={classes.version}>v2.0.5</span></h1>
+          <p className={classes.heroDesc}>
+            Make it easier to use Eva Icons in your React projects.
+          </p>
+          <div className={classes.tutorial}>
+            <div>Tutorial</div>
+            <div className={classes.tutorialBlock}>
+              <div className={classes.code}>
+                <p>Install</p>
+                <pre className={classes.pre}>
+                  {`
+  npm install @forefront-ux/react-eva-icons
+
+  or
+  
+  yarn add @forefront-ux/react-eva-icons
+                  `}
+                </pre>
+              </div>
+              <div className={classes.code}>
+                <p>Import it into your React code</p>
+                <pre className={classes.pre}>
+                  {`
+  ...
+  
+  import {
+    EvaActivity,
+    EvaActivityOutline,
+  } from '@forefront-ux/react-eva-icons';
+  import '@forefront-ux/react-eva-icons/index.css';
+  
+  ...
+
+  const Icon = () => (
+    <EvaActivity
+      color="#3F51B5"
+      size={48}
+      animation="zoom"
+    />
+  )
+
+  const Icon = () => (
+    <EvaActivityOutline
+      color="#3F51B5"
+      size={48}
+      animation="zoom"
+    />
+  )
+
+  ...
+                  `}
+                </pre>
+              </div>
+              <div className={classes.code}>
+                <p>Props</p>
+                <pre className={classes.pre}>
+                  {`
+  color: PropTypes.string,
+  size: PropTypes.number,
+  animation: PropTypes.oneOf([
+    'zoom',
+    'pulse',
+    'shake',
+    'flip'
+  ])
+                  `}
+                </pre>
+              </div>
+            </div>
+          </div>
+        </div>
         <Icons />
       </main>
       <footer className={classes.footer}>
